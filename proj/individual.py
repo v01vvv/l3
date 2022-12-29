932aa01fad9cd8909ef1fd67bbab9f01e6c44be0
v01vvv <nosirovdilshod1981@gmail.com>
2022-12-29 16:17:06 +0300

    1

---
+++ proj/individual.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+if __name__ == '__main__':
+    N = int(input("N "))
+    if N == 1:
+        print(f"я купил {N} карандаш")
+    elif N < 5:
+        print(f"я купил {N} карандаша")
+    elif N < 11:
+        print(f"я купил {N} карандашей")
